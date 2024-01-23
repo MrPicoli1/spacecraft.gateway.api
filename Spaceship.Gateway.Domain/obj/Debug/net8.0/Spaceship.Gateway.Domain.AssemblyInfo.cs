feb96c474c33b7db05c06a138eafe3a1a6f287c3
Guilherme Picoli <83010918+MrPicoli1@users.noreply.github.com>
2024-01-23 19:00:00 -0300

    initial 2

--- Spaceship.Gateway.Domain/obj/Debug/net8.0/Spaceship.Gateway.Domain.AssemblyInfo.cs
+++ Spaceship.Gateway.Domain/obj/Debug/net8.0/Spaceship.Gateway.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spaceship.Gateway.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b0803b9277f1eb64254d4565f667f320bddbdbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spaceship.Gateway.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spaceship.Gateway.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Spaceship.Gateway.Shared/obj/Debug/net8.0/Spaceship.Gateway.Shared.AssemblyInfo.cs
+++ Spaceship.Gateway.Shared/obj/Debug/net8.0/Spaceship.Gateway.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spaceship.Gateway.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b0803b9277f1eb64254d4565f667f320bddbdbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spaceship.Gateway.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spaceship.Gateway.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
